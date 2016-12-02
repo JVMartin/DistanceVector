ba24614a7fddc7eb7438aedd4bab4a7bfd5a50d7
Jacob Martin <mindnumb@gmail.com>
2016-12-02 12:14:29 -0800

    Clarifying comments, better output formatting

--- src/Graph.php
+++ src/Graph.php
@@ -4,24 +4,31 @@ namespace DV;
 
 class Graph
 {
+	/**
+	 * An array of Node objects.
+	 */
 	private $nodes = [];
 
+	/**
+	 * Graph constructor.
+	 *
+	 * @param array $labels An array of node labels.
+	 */
 	public function __construct(array $labels)
 	{
+		// Loop through the array of node labels and create a new node for each one.
 		foreach ($labels as $label) {
 			$this->nodes[$label] = new Node($label, $labels);
 		}
 	}
 
-	public function __toString()
-	{
-		$s = '';
-		foreach ($this->nodes as $node) {
-			$s .= $node->label . ': ' . $node . "\n";
-		}
-		return $s;
-	}
-
+	/**
+	 * Make nodes A and B neighbors of each other.
+	 *
+	 * @param $a string The label of node A.
+	 * @param $b string The label of node B.
+	 * @param $weight int The weight of the path between them.
+	 */
 	public function addEdge($a, $b, $weight)
 	{
 		$nodeA = $this->nodes[$a];
@@ -31,20 +38,34 @@ class Graph
 		$nodeB->addNeighbor($nodeA, $weight);
 	}
 
+	/**
+	 * Run through the algorithm once.
+	 *
+	 * @return bool If there were updates, return true.  Otherwise, return false.
+	 */
 	public function iterate()
 	{
 		$updates = false;
+
+		// First, let each node update its neighbors with its DV.
 		foreach ($this->nodes as $node) {
 			$node->updateNeighbors();
 		}
+
+		// Next, let each node recalculate its DV.
 		foreach ($this->nodes as $node) {
 			if ($node->recalc()) {
 				$updates = true;
 			}
 		}
+
+		// If there were updates, return true.  Otherwise, return false.
 		return $updates;
 	}
 
+	/**
+	 * Step through the algorithm, displaying the new DV's at each step.
+	 */
 	public function showSteps()
 	{
 		$i = 0;
@@ -53,5 +74,18 @@ class Graph
 			echo "\n---------------------------------\n";
 			echo $this;
 		} while($this->iterate());
+	}
+
+	public function __toString()
+	{
+		$s = " \t";
+		foreach ($this->nodes as $node) {
+			$s .= $node->label . "\t";
+		}
+		$s .= "\n";
+		foreach ($this->nodes as $node) {
+			$s .= $node->label . "\t" . $node . "\n";
+		}
+		return $s;
 	}
 }
--- src/Node.php
+++ src/Node.php
@@ -4,25 +4,59 @@ namespace DV;
 
 class Node
 {
-	private $labels;
+	/**
+	 * A string containing this Node's label.  (e.g. 'A')
+	 */
 	public $label;
-	private $dv        = [];
+
+	/**
+	 * This node's distance vector.  Is of the form:
+	 * ['A' => INF, 'B' => 2, ...]
+	 */
+	private $dv = [];
+
+	/**
+	 * An array of references to this Node's neighbor Nodes.
+	 */
 	private $neighbors = [];
+
+	/**
+	 * An array of this Node's neighbors' distance vectors.
+	 * (Each distance vector is the same form as the distance vector property above.)
+	 */
 	private $nDVs      = [];
 
+	/**
+	 * An array of N label strings, one for each node in the graph.
+	 */
+	private $labels = [];
+
+	/**
+	 * Node constructor.
+	 *
+	 * @param $label string This Node's label.
+	 * @param array $labels An array of N label strings, one for each node in the graph.
+	 */
 	public function __construct($label, array $labels)
 	{
 		$this->labels = $labels;
-		$this->label = $label;
+		$this->label  = $label;
 
 		// Initialize our distance vector to infinity.
 		foreach ($this->labels as $l) {
 			$this->dv[$l] = INF;
 		}
 
+		// Except - set the distance to ourself to 0.
 		$this->dv[$this->label] = 0;
 	}
 
+	/**
+	 * Add a neighbor to this node.
+	 *
+	 * @param Node $node The reference to the neighbor.
+	 * @param $weight int The cost of the path between this node and the neighbor.
+	 */
 	public function addNeighbor(Node $node, $weight)
 	{
 		$this->neighbors[$node->label] = $node;
@@ -34,6 +68,9 @@ class Node
 		}
 	}
 
+	/**
+	 * Give our neighbor nodes our distance vector.
+	 */
 	public function updateNeighbors()
 	{
 		foreach ($this->neighbors as $neighbor) {
@@ -41,15 +78,29 @@ class Node
 		}
 	}
 
+	/**
+	 * Receive a distance vector from a neighbor.
+	 *
+	 * @param $label string The label of the neighbor.
+	 * @param array $dv The neighbor's distance vector.
+	 */
 	private function updateDV($label, array $dv)
 	{
 		$this->nDVs[$label] = $dv;
 	}
 
+	/**
+	 * Recalculate this node's distance vector based on the neighbors' distance vectors.
+	 *
+	 * @return bool True if there were changes/updates; false otherwise.
+	 */
 	public function recalc()
 	{
 		$updates = false;
+
+		// Loop through each neighbor's distance vector.
 		foreach ($this->nDVs as $nLabel => $nDV) {
+			// Calculate the cost.  If it's less than the current cost, update our distance vector.
 			foreach ($nDV as $label => $costFromNeighbor) {
 				$cost = $nDV[$this->label] + $costFromNeighbor;
 				if ($cost < $this->dv[$label]) {
@@ -63,6 +114,6 @@ class Node
 
 	public function __toString()
 	{
-		return implode(', ', $this->dv);
+		return implode("\t", $this->dv);
 	}
 }
